056a888ccd08851cf7a5033061646e49111a86dd
ZJBarney <ZJBarney@gmail.com>
2023-05-16 18:58:20 +0000

    Add files via upload

--- PiSkyCam.py
+++ PiSkyCam.py
@@ -1,8 +1,15 @@
 from picamera import PiCamera
 from os import system
+from time import sleep
 import datetime
 import os.path
-from time import sleep
+import socket
+
+NMEA_data = ""
+lat_data = ""
+long_data = ""
+DDM_lat = ""
+DDM_long = ""
 
 #sleep(60)  #Time delay to allow system to boot and update time/date
        
@@ -19,6 +26,11 @@ def add_log(log_info, x):
         f.close()
         f = open(save_path, 'w')
         f.writelines(lines)
+    if x == 2:
+        f2 = open(path + '/' + time_format + 'gps_log.txt', 'a')
+        f2.write(log_info)
+        f2.close        
+
 
 def take_pic():
     dateraw= datetime.datetime.now()
@@ -26,6 +38,34 @@ def take_pic():
     time_format = dateraw.strftime('%H%M%S')
     picture_filename = path + '/' + time_format +'.jpg'
     camera.capture(picture_filename)
+
+def gps_location():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        client_socket.connect(("192.168.50.1", 60660)) #IP and socket for Pepwave GPS data
+    except:
+        add_log("GPS invalid, GPS lost, socket closed\n", 2)
+        print("GPS socket closed")
+    else:
+        data = client_socket.recv(1024) #recieve data from socket
+        if len(data) > 0: #check for data
+            NMEA_data=data.decode('ascii')
+            lines = NMEA_data.splitlines() #read line by line
+            for line in lines: # iterate over each line
+                gpsstring = line.split(',')
+                if gpsstring[0] == '$GPRMC' :
+                    if len(gpsstring[1]) > 6 and len(gpsstring[3]) > 6 and len(gpsstring[5]) > 6:
+                        lat_data = gpsstring[3] + gpsstring[4]
+                        long_data = gpsstring[5] + gpsstring[6]
+                        DDM_lat = " Lat:" + lat_data[:2] + " " +  lat_data[2:8] + lat_data[-1]
+                        DDM_long = " Long:" + long_data[:3] + " " + long_data[3:9] + long_data[-1]
+                        print("Time: " + str(dateraw) + DDM_lat + DDM_long)
+                        add_log("Time:" + str(dateraw) + DDM_lat + DDM_long + '\n', 2)
+                    else:
+                        print("Time: " + str(dateraw) + " GPS invalid")
+                        add_log("Time: " + str(dateraw) + " GPS invalid\n", 2)
+    finally:
+        client_socket.close()
     
 dateraw= datetime.datetime.now()
 date_time_format = dateraw.strftime('%Y%m%d')
@@ -58,12 +98,13 @@ add_log('Pictures starting', 0)
 ### The Pi camera needs time to focus and adjust exposure and white balance
 pic_count = 1
 while True:
-    dateraw=datetime.datetime.now()
+    dateraw = datetime.datetime.now()
     time_sync = dateraw.strftime('%S')
     time_int = int(time_sync)
+    gps_location()
     if time_int % 3 == 0: #takes photos every 3 seconds
         take_pic()
-        sleep(1)
         pic_count += 1
+        sleep(1) #allows time to write photo
         add_log('Picture count: ' + str(pic_count), 1)
 ########################################################    
---
+++ extDriveReboot.py
@@ -0,0 +1,33 @@
+#check for external drive
+#if drive is not present, reboot RPi
+from time import sleep
+from gpiozero import CPUTemperature
+import os
+import os.path
+import datetime
+import threading
+
+tdelay = 100 #time delay between checking for external drive and cpu temperature
+             #1000 = 10 minutes, 100 = 1 minute, 10 = 10 seconds
+
+extdrive_path = '/media/pi/extdrive'
+isdir = os.path.isdir(extdrive_path)
+
+while True:
+    dateraw= datetime.datetime.now()
+    hms_time = dateraw.strftime('%H%M%S')
+    sleep(1)
+    if int(hms_time) % 10 == 0:
+        if isdir is True:
+            print('External Drive Present')
+            temp = str(CPUTemperature())
+            temp = temp.partition("=")[2]
+            print(temp[:-1] + " deg C")
+            f = open('/home/pi/Desktop/templog.txt', 'a')
+            f.write(str(dateraw) + ' ' + temp[:-1] + '\n')
+            f.close
+        else:
+            f = open('/home/pi/Desktop/templog.txt', 'a')
+            f.write('Shutting down at ' + str(dateraw) + '\n')
+            f.close
+            os.system('sudo shutdown -r now')
